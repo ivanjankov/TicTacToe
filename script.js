95409c17478bd1adc4d90194daeaec01c352afb6
Ivan Jankov <ivanjankov07@gmail.com>
2022-01-28 13:18:59 +0100

    Fill squares html based on which player is playing

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
+	let DOMStrings = {
+		square: '.square',
+	};
 
+	function inputHtmlBasedOnPlayer() {
+		let player = 1;
+		let squares = document.querySelectorAll(DOMStrings.square);
+
+		squares.forEach((el) => {
+			el.addEventListener(
+				'click',
+				() => {
+					if (player == 1) {
+						el.innerHTML = 'X';
+						player = 0;
+					} else if (player == 0) {
+						el.innerHTML = 'O';
+						player = 1;
+					}
+				},
+				{ once: true }
+			);
+		});
+	}
+
+	inputHtmlBasedOnPlayer();
+});
